13375fc5c8ccd84cc9d64c6783b88d8050d97370
Daniel Zegerson <danizeger283@gmail.com>
2023-03-24 19:35:02 +0300

    Sign Up Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,28 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './home/button/button.component';
 import { ProjectCardComponent } from './projects/project-card/project-card.component';
 import { SectionTitleComponent } from './home/section-title/section-title.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { ProjectPageComponent } from './projects/project-page/project-page.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { AboutPageComponent } from './about/about-page/about-page.component';
 import { ProjectsModule } from './projects/projects.module';
 import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ProjectsModule,
     HomeModule,
-    AboutModule
+    AboutModule,
+    AuthModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
